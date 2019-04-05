276e0758c068bb1f7ff3622f18cf0d15fd73e337
Ryan David Forsyth <forsyth.r@gmail.com>
2019-04-05 16:47:54 -0400

    Problem 7 complete

--- problems/problem7.js
+++ problems/problem7.js
@@ -1,13 +1,25 @@
-let verifyEquals = require('./verify-equals.js');
+let verifyEquals = require("./verify-equals.js");
 
-// we need 7 test cases. 
+// we need 7 test cases.
 let inputs = [
-  
-]
+  ["YeahBuddy", 2],
+  ["Trythisoneoutforsize", 3],
+  [" ( ( ) ) ", 4],
+  ["3", 3],
+  [666, 6],
+  ["4", -1],
+  ["Will it run???"]
+];
 
 let outputs = [
-  
-]
+  "YeahBuddyYeahBuddy",
+  "TrythisoneoutforsizeTrythisoneoutforsizeTrythisoneoutforsize",
+  " ( ( ) )  ( ( ) )  ( ( ) )  ( ( ) ) ",
+  "333",
+  undefined,
+  "",
+  undefined
+];
 
 /*
 The function input is an array as input. The first element of the array is a string. The second is a number.
@@ -15,20 +27,34 @@ Make this function return the string repeated as many times as specified by the 
 If a negative number or zero is specified, return an empty string. If any invalid parameters are supplied return undefined.
 
 For example:
-
-f(["foo", 3]) // "foofoofoo"
-f(["fo", 3]) // "fofofo"
-f(["foo", -1]) // ""
 */
+
 function f(arr) {
-    
+  if (typeof arr[0] != "string" || typeof arr[1] != "number") return undefined;
+
+  if (arr.length != 2) return undefined;
+
+  if (arr[1] === -1) return "";
+
+  let ret = "";
+
+  let repeat = arr[1];
+
+  let str = arr[0];
+
+  for (let x = 0; x < repeat; x++) {
+    ret += str;
+    console.log(ret);
+  }
+
+  return ret;
 }
 
 function runTest(i) {
-    if(i > inputs.length) throw new Error("You do not have enough test cases");
-    let expected = outputs[i];
-    let actual = f(inputs[i]);
-    verifyEquals(expected, actual)
+  if (i > inputs.length) throw new Error("You do not have enough test cases");
+  let expected = outputs[i];
+  let actual = f(inputs[i]);
+  verifyEquals(expected, actual);
 }
 
 runTest(0);
@@ -38,4 +64,6 @@ runTest(3);
 runTest(4);
 runTest(5);
 runTest(6);
-console.log("All tests passed for " + __filename)
+console.log("All tests passed for " + __filename);
+
+//Checked!
